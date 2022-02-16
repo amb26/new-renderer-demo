ed93d7cfaa085b6526b4c829519fc9261ca35521
Antranig Basman <antranig.basman@colorado.edu>
2022-02-16 11:25:13 +0000

    FLUID-5047: Dependency update to pre-release Infusion 4 and fix for ToC Schema grade

--- preferences/tableOfContents/js/TableOfContents.js
+++ preferences/tableOfContents/js/TableOfContents.js
@@ -14,7 +14,7 @@ https://github.com/fluid-project/infusion/raw/main/Infusion-LICENSE.txt
 "use strict";
 
 fluid.defaults("fluid.prefs.schemas.tableOfContents", {
-    gradeNames: ["fluid.prefs.schemas"],
+    gradeNames: ["fluid.prefs.schema"],
     schema: {
         "fluid.prefs.tableOfContents": {
             "type": "boolean",
